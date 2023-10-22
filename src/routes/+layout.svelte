465f0834ad20f4459096766d2d3da291aed9bff5
Jasper Mayone <jasper.mayone@singlefeather.com>
2023-10-21 21:10:17 -0400

    add mailchimp

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<div class="container">
+  <slot />
+</div>
+
+<style>
+  .container {
+    padding: 50px 0 100px 0;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,189 @@
+<main>
+  <h1 class="title">Welcome to Cyaneus.</h1>
+
+  <p class="description">
+    A social media platform with user privacy and trasparency at it's forefront.
+  </p>
+<spacer/>
+  <div class="box">
+    <div id="mc_embed_shell">
+      <link
+        href="//cdn-images.mailchimp.com/embedcode/classic-061523.css"
+        rel="stylesheet"
+        type="text/css"
+      />
+      <style type="text/css">
+        #mc_embed_signup{background:#fff; false;clear:left; font:14px Helvetica,Arial,sans-serif; width: 600px;}
+        /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+           We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+      </style>
+      <div id="mc_embed_signup">
+        <form
+          action="https://cyaneus.us21.list-manage.com/subscribe/post?u=8ec1e26257e935f25a5c61caa&amp;id=5b2aac30ef&amp;f_id=006eafe1f0"
+          method="post"
+          id="mc-embedded-subscribe-form"
+          name="mc-embedded-subscribe-form"
+          class="validate"
+          target="_blank"
+        >
+          <div id="mc_embed_signup_scroll">
+            <h2>
+              Interested in Cyaneus? Sign up to be the first to know when we go
+              live!
+            </h2>
+            <div class="indicates-required">
+              <span class="asterisk">*</span> indicates required
+            </div>
+            <div class="mc-field-group">
+              <label for="mce-EMAIL"
+                >Email Address <span class="asterisk">*</span></label
+              ><input
+                type="email"
+                name="EMAIL"
+                class="required email"
+                id="mce-EMAIL"
+                required=""
+                value=""
+              />
+            </div>
+            <div class="mc-field-group">
+              <label for="mce-FNAME"
+                >First Name <span class="asterisk">*</span></label
+              ><input
+                type="text"
+                name="FNAME"
+                class="required text"
+                id="mce-FNAME"
+                required=""
+                value=""
+              />
+            </div>
+            <div class="mc-field-group">
+              <label for="mce-LNAME"
+                >Last Name <span class="asterisk">*</span></label
+              ><input
+                type="text"
+                name="LNAME"
+                class="required text"
+                id="mce-LNAME"
+                required=""
+                value=""
+              />
+            </div>
+            <div id="mce-responses" class="clear foot">
+              <div
+                class="response"
+                id="mce-error-response"
+                style="display: none;"
+              />
+              <div
+                class="response"
+                id="mce-success-response"
+                style="display: none;"
+              />
+            </div>
+            <div aria-hidden="true" style="position: absolute; left: -5000px;">
+              /* real people should not fill this in and expect good things - do
+              not remove this or risk form bot signups */
+              <input
+                type="text"
+                name="b_8ec1e26257e935f25a5c61caa_5b2aac30ef"
+                tabindex="-1"
+                value=""
+              />
+            </div>
+            <div class="optionalParent">
+              <div class="clear foot">
+                <input
+                  type="submit"
+                  name="subscribe"
+                  id="mc-embedded-subscribe"
+                  class="button"
+                  value="Subscribe"
+                />
+                <p style="margin: 0px auto;">
+                  <a
+                    href="http://eepurl.com/ipn9TQ"
+                    title="Mailchimp - email marketing made easy and fun"
+                    ><span
+                      style="display: inline-block; background-color: transparent; border-radius: 4px;"
+                      ><img
+                        class="refferal_badge"
+                        src="https://digitalasset.intuit.com/render/content/dam/intuit/mc-fe/en_us/images/intuit-mc-rewards-text-dark.svg"
+                        alt="Intuit Mailchimp"
+                        style="width: 220px; height: 40px; display: flex; padding: 2px 0px; justify-content: center; align-items: center;"
+                      /></span
+                    ></a
+                  >
+                </p>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+      <script
+        type="text/javascript"
+        src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"
+      ></script><script type="text/javascript">
+        (function ($) {
+          window.fnames = new Array();
+          window.ftypes = new Array();
+          fnames[0] = "EMAIL";
+          ftypes[0] = "email";
+          fnames[1] = "FNAME";
+          ftypes[1] = "text";
+          fnames[2] = "LNAME";
+          ftypes[2] = "text";
+          fnames[3] = "ADDRESS";
+          ftypes[3] = "address";
+          fnames[4] = "PHONE";
+          ftypes[4] = "phone";
+          fnames[5] = "BIRTHDAY";
+          ftypes[5] = "birthday";
+        })(jQuery);
+        var $mcj = jQuery.noConflict(true);
+      </script>
+    </div>
+  </div>
+</main>
+
+<style>
+  :global(body) {
+    background-image: url("/background.svg");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: fill;
+    background-attachment: fixed;
+    min-height: 100vh;
+    z-index: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  main {
+    padding: 2rem 0;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    z-index: 1;
+    background-color: azure;
+  }
+
+  .title {
+    margin: 0;
+    line-height: 1.15;
+    font-size: 4rem;
+  }
+
+  .title,
+  .description {
+    text-align: center;
+  }
+
+  .description {
+    line-height: 1.5;
+    font-size: 1.5rem;
+  }
+</style>
